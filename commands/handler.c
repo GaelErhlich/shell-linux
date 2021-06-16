45cdc2b8aa2030877e35df3548a69b2aff3529a2
GaelErhlich <idiotdu92@gmail.com>
2021-06-16 13:47:22 +0200

    Added datastructures

---
+++ commands/handler.c
@@ -0,0 +1,18 @@
+#include "headers/handler.h"
+
+#include <string>
+
+#include "" // job.h
+
+
+using std::string;
+
+
+void handleCommand(string cmd) {
+	
+}
+
+
+int main(int argc, char* argv[]) {
+	
+}
